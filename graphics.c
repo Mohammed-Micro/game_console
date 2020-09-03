14ae2e8aae1ef4c54ded89d781d860295e79688b
Mohammed-Micro <theghost171297@gmail.com>
2020-09-03 18:25:37 +0200

    graphics

--- graphics.c
+++ graphics.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
 #include "graphics.h"
 #include "vga.h"
 
-Window* newwin(uint16_t height, uint16_t width, uint16_t y_beg, uint16_t x_beg){
+Window newwin(uint16_t height, uint16_t width, uint16_t y_beg, uint16_t x_beg){
 
-	Window *win  = {
-		.win_buff_ptr = (char*) malloc(height * width),
+	Window win  = {
+		.win_buff = (char*) malloc(height * width),
 		.height = height,
 		.width = width,
 		.y_beg = y_beg,
@@ -19,15 +20,63 @@ void wrefresh(Window *win){
 
 	while(!in_backporch());
 
-	uint16_t y_end = y_beg + win->height;
-	uint16_t x_end = x_beg + win->width;
+	uint16_t y_end = win->y_beg + win->height;
+	uint16_t x_end = win->x_beg + win->width;
 	uint16_t cnt = 0;
 
 	for(uint16_t i = win->y_beg; i < y_end; i++){
 		
 		for(uint16_t j = win->x_beg; j < x_end; j++){
 
-			vga_buff[i][j] = win->win_buff_ptr[cnt++];
+			vga_buff[i][j] = win->win_buff[cnt++];
 		}
 	}
 }
+
+void box(Window *win, uint8_t color, uint8_t thickness){
+
+	uint16_t thresh = thickness * win->width;
+	uint8_t clr = color | (color << 4);
+
+	//upper border
+	
+	for(uint16_t i = 0; i < thresh; i++){
+		win->win_buff[i] = clr;
+	}
+
+	thresh = win->width * win->height;
+
+	//lower border
+	
+	for(uint16_t i = thresh - thickness * win->width; i < thresh; i++){
+		win->win_buff[i] = clr;
+	}
+
+	thresh = (win->height - thickness) * win->width;
+
+	//left border
+	
+	for(uint16_t j = 0; j < thickness; j++){
+		for(uint16_t i = win->width * thickness; i < thresh; i += win->width){
+		       if(j & 1){
+			       win->win_buff[i + j/2] = (win->win_buff[i + j/2] & 0x0f) | (color << 4);
+		       }
+		       else{
+			       win->win_buff[i + j/2] = (win->win_buff[i + j/2] & 0xf0) | color;
+		       }
+		}
+	}
+
+	//right border
+	for(uint16_t j = win->width - thickness; j < win->width; j++){
+                for(uint16_t i = win->width * thickness; i < thresh; i += win->width){
+                       if(j & 1){
+                               win->win_buff[i + j/2] = (win->win_buff[i + j/2] & 0x0f) | (color << 4);
+                       }
+                       else{
+                               win->win_buff[i + j/2] = (win->win_buff[i + j/2] & 0xf0) | color;
+                       }
+                }
+        }
+
+}		
--- graphics.h
+++ graphics.h
@@ -1,9 +1,22 @@
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
 
+typedef unsigned short int uint16_t;
+typedef unsigned char uint8_t;
+
+enum Colors{
+	COLOR_BLACK = 0X00,
+	COLOR_RED = 0X1,
+	COLOR_GREEN = 0X02,
+	COLOR_BLUE = 0X08,
+	COLOR_YELLOW = 0X03,	//RED AND GREEN
+	COLOR_CYAN = 0X0A,	//GREEN AND BLUE
+	COLOR_MAGENTA = 0X09,	//RED AND BLUE
+	COLOR_WHITE = 0X0F	//RED, GREEN AND BLUE
+};
 
 typedef struct __window{
-	char *win_buff_ptr;
+	char *win_buff;
 	uint16_t height;
 	uint16_t width;
 	uint16_t y_beg;
@@ -12,7 +25,8 @@ typedef struct __window{
 }Window;
 
 
-Window* newwin(uint16_t, uint16_t, uint16_t, uint16_t);
+Window newwin(uint16_t, uint16_t, uint16_t, uint16_t);
 void wrefresh(Window*);
+void box(Window*, uint8_t, uint8_t);
 
 #endif
