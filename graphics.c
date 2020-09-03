efcb96efdc7fe60eaa39267c06bf2bc3d54e99ec
Mohammed-Micro <theghost171297@gmail.com>
2020-09-03 03:24:58 +0200

    graphics

--- graphics.c
+++ graphics.c
@@ -1,10 +1,10 @@
 #include "graphics.h"
-
+#include "vga.h"
 
 Window* newwin(uint16_t height, uint16_t width, uint16_t y_beg, uint16_t x_beg){
 
 	Window *win  = {
-		.win_buff_ptr = malloc(height * width),
+		.win_buff_ptr = (char*) malloc(height * width),
 		.height = height,
 		.width = width,
 		.y_beg = y_beg,
@@ -13,4 +13,21 @@ Window* newwin(uint16_t height, uint16_t width, uint16_t y_beg, uint16_t x_beg){
 	};
 
 	return win;
+}
+
+void wrefresh(Window *win){
+
+	while(!in_backporch());
+
+	uint16_t y_end = y_beg + win->height;
+	uint16_t x_end = x_beg + win->width;
+	uint16_t cnt = 0;
+
+	for(uint16_t i = win->y_beg; i < y_end; i++){
+		
+		for(uint16_t j = win->x_beg; j < x_end; j++){
+
+			vga_buff[i][j] = win->win_buff_ptr[cnt++];
+		}
+	}
 }
