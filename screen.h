d5a10bea815ed1f3c3ebe46e057d84e1c59ebe0d
Mohammed-Micro <44850046+Mohammed-Micro@users.noreply.github.com>
2020-09-19 23:31:40 +0200

    Update screen.h

--- screen.h
+++ screen.h
@@ -13,7 +13,8 @@ void show_new_piece(Window*, Piece*);
 void move_piece_down(Window*, Piece*);
 void move_piece_right(Window*, Piece*);
 void move_piece_left(Window*, Piece*);
-void rotate_piece(Window*, Piece*);
+void rotate_piece_cwise(Window*, Piece*);
+void rotate_piece_ccwise(Window *win, Piece *p)
 int8_t try_move_down(Window*, Piece*);
 int8_t try_move_right(Window*, Piece*);
 void show_title(void);
